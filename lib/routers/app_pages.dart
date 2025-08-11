9a001d24cbdd2bc3adc311e5a032487205be6796
Godtasan552 <werawat7903@gmail.com>
2025-08-11 20:11:22 +0700

    delete login and register

--- lib/routers/app_pages.dart
+++ lib/routers/app_pages.dart
@@ -2,9 +2,7 @@ import 'package:get/get.dart';
 import 'app_routes.dart';
 
 import '../screens/splash_screen.dart';
-import '../screens/login.dart';
-import '../screens/regis.dart';
-import '../screens/forget_pass.dart';
+import '../screens/home_screen.dart';
 
 class AppPages {
   AppPages._();
@@ -15,30 +13,6 @@ class AppPages {
       name: AppRoutes.SPLASH,
       page: () => const SplashScreen(),
       transition: Transition.fadeIn,
-      transitionDuration: const Duration(milliseconds: 300),
-    ),
-
-    // Login Page
-    GetPage(
-      name: AppRoutes.LOGIN,
-      page: () => const LoginScreen(),
-      transition: Transition.fadeIn,
-      transitionDuration: const Duration(milliseconds: 300),
-    ),
-
-    // Register Page
-    GetPage(
-      name: AppRoutes.REGISTER,
-      page: () => const RegisterScreen(),
-      transition: Transition.rightToLeft,
-      transitionDuration: const Duration(milliseconds: 300),
-    ),
-
-    // Forget Password Page
-    GetPage(
-      name: AppRoutes.FORGET_PASSWORD,
-      page: () => const ForgetPasswordScreen(),
-      transition: Transition.cupertino,
       transitionDuration: const Duration(milliseconds: 300),
     ),
 
